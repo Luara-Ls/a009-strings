5f2ff86f185836351c579ed589737dd2e31cf656
Luara <luaraap7@gmail.com>
2022-07-26 18:03:16 -0300

    exercicio1 finalizado

---
+++ exercicio1/script.js
@@ -0,0 +1,8 @@
+
+const nome = prompt("Qual seu nome?")
+
+const comida1 = "hambúrguer";
+const comida2 = "baião de dois";
+const comida3 =  "pizza";
+
+console.log('Estas são as comidas favoritas de ' +  nome + ':' + comida1 + ', '  +  comida2 + ' e ' + comida3)
